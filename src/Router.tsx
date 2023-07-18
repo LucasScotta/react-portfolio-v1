f4eee4477161e017279150c810b9e4b37590891a
LucasScotta <lucasmanuelscotta@gmail.com>
2023-07-17 22:01:15 -0300

    suspense added to Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { lazy, Suspense } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { projectsPath } from './constants'
-
+const Home = lazy(() => import('./Projects/Home/Home'))
 export const Router = () => {
   const { HOME } = projectsPath
   return (
     <BrowserRouter>
-      <Routes>
-      </Routes>
+      <Suspense fallback={<>LOADING...</>}>
+        <Routes>
+          <Route path={HOME} element={<Home />} />
+          <Route path="*" element={<Navigate to={HOME} replace />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
