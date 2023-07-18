b303696c05180dd1e33c1dba8efced1450481c2c
LucasScotta <lucasmanuelscotta@gmail.com>
2023-07-18 15:42:17 -0300

    parsing localStorage in try/catch added

--- src/Projects/Home/utils/localStorage.ts
+++ src/Projects/Home/utils/localStorage.ts
@@ -3,11 +3,22 @@ import { defaultLanguage } from '../constants'
 
 
 export const getLanguage = () => {
+  const isLanguage = (value: unknown): value is Language => typeof value === 'string' && (value === 'es' || value === 'en')
 
-  const language = localStorage.getItem('language')
-  if (language) return JSON.parse(language) as Language
+  try {
+    const item = localStorage.getItem('language')
+    if (!item) return defaultLanguage
 
-  return defaultLanguage
+    const language: unknown = JSON.parse(item)
+    if (!isLanguage(language)) return defaultLanguage
+
+    return isLanguage(language) ? language : defaultLanguage
+  }
+  catch (e) {
+    console.log('Error parsing language\n', e)
+    persistLanguage(defaultLanguage)
+    return defaultLanguage
+  }
 }
 
 export const persistLanguage = (language: Language) => localStorage.setItem('language', JSON.stringify(language))
