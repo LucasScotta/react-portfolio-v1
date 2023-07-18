604094aa6ef7d29ce35ad2a099dbca3dd0733b12
LucasScotta <lucasmanuelscotta@gmail.com>
2023-07-18 18:16:48 -0300

    constants updated with FR language

--- src/Projects/Home/constants.ts
+++ src/Projects/Home/constants.ts
@@ -2,12 +2,14 @@ import { Language } from './Model'
 
 type DescriptionList = {
   'es': string[],
-  'en': string[]
+  'en': string[],
+  'fr': string[]
 }
 
 export const defaultLanguage: Language = 'es'
-
+export const languages: Language[] = ['es', 'en', 'fr']
 export const descriptionList: DescriptionList = {
-  'es': ['Dev', 'Argentino', 'gamer'],
-  'en': ['Dev', 'Argentinian', 'gamer']
+  'es': ['desarrollador', 'Argentino', 'gamer'],
+  'en': ['developer', 'Argentinian', 'gamer'],
+  'fr': ['développeur', 'argentin', 'joueur']
 }
