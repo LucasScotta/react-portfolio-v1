7fe774013014dac1bcfa2687153de4023ebef41c
LucasScotta <lucasmanuelscotta@gmail.com>
2023-07-18 12:59:54 -0300

    list of description added to constants

--- src/Projects/Home/constants.ts
+++ src/Projects/Home/constants.ts
@@ -1,3 +1,13 @@
 import { Language } from './Model'
 
+type DescriptionList = {
+  'es': string[],
+  'en': string[]
+}
+
 export const defaultLanguage: Language = 'es'
+
+export const descriptionList: DescriptionList = {
+  'es': ['Dev', 'Argentino', 'gamer'],
+  'en': ['Dev', 'Argentinian', 'gamer']
+}
