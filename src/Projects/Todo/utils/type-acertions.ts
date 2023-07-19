b761df6f8785980621222005635712b0dd1a0d97
LucasScotta <lucasmanuelscotta@gmail.com>
2023-07-19 02:02:19 -0300

    local storage utils removed from Todo Project and implemented local storage custom hook

---
+++ src/Projects/Todo/utils/type-acertions.ts
@@ -0,0 +1,20 @@
+import { Todo } from '../Models'
+
+const isString = (s: unknown): s is string => typeof s === 'string'
+const isBoolean = (b: unknown): b is boolean => Boolean(b) === b
+const isTodo = (todo: unknown): todo is Todo => (
+  !!todo &&
+  typeof todo === 'object' && !Array.isArray(todo) &&
+  'id' in todo && isString(todo.id) &&
+  'name' in todo && isString(todo.name) &&
+  'finished' in todo && isBoolean(todo.finished) &&
+  'description' in todo && isString(todo.description)
+)
+
+export const isTodoArray = (todos: unknown): todos is Todo[] => {
+  if (!todos || !Array.isArray(todos)) return false
+  for (const todo of todos) {
+    if (!isTodo(todo)) return false
+  }
+  return true
+}
