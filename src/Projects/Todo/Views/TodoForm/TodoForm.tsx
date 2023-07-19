ce388781921590bdd20fbe67d5aa787d7ae630d8
LucasScotta <lucasmanuelscotta@gmail.com>
2023-07-19 16:20:50 -0300

    Todo Project resposive added

--- src/Projects/Todo/Views/TodoForm/TodoForm.tsx
+++ src/Projects/Todo/Views/TodoForm/TodoForm.tsx
@@ -1,11 +1,17 @@
 import { useContext } from 'react'
 import { TodoContext } from '../../Context/TodoContext'
+import './style.css'
 
 export const TodoForm = () => {
   const { addTodo } = useContext(TodoContext)
-  return <form onSubmit={addTodo}>
-    <input type='text' name='name' />
-    <textarea name='description' />
+  return <form className="Todo-Form" onSubmit={addTodo}>
+    <input
+      type='text'
+      name='name'
+      placeholder='Desarrollar una App en React' />
+    <textarea
+      name='description'
+      placeholder={'Desarrollar una App en React usando Custom Hooks y React-Router, o también, forkear este proyecto y customizarlo 🤜🤛.\nAbrí esta página en una nueva pestaña o ventana, agrega una tarea y volve a este navegador a ver la magia'} />
     <button>Add</button>
   </form>
 }
