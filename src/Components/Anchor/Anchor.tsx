c65394df61c537b43c0e61af5fc3cc6441335d9f
LucasScotta <lucasmanuelscotta@gmail.com>
2023-07-19 16:20:31 -0300

    Global Anchor Component functionality fixed

--- src/Components/Anchor/Anchor.tsx
+++ src/Components/Anchor/Anchor.tsx
@@ -4,13 +4,19 @@ import { useNavigate } from 'react-router-dom'
 interface AnchorProps {
   children: string
   path: string
+  target?: string
 }
 
-export const Anchor: FC<AnchorProps> = ({ children, path }) => {
+export const Anchor: FC<AnchorProps> = ({ children, path, target }) => {
   const navigate = useNavigate()
+  if (target) return <a href={path} target={target} />
   const anchorClick = (e: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>) => {
-    e.preventDefault()
-    navigate(path, { replace: true })
+    if (e.button === 0) {
+      // Primary click, left as default
+      e.preventDefault()
+      window.history.pushState(null, '', path)
+      navigate(path, { replace: true })
+    }
   }
   return <a href={path} onClick={anchorClick}>{children}</a>
 }
