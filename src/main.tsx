01c9c49802b15d0a8221d2e3410fbac8c7391aa1
LucasScotta <lucasmanuelscotta@gmail.com>
2023-07-19 15:48:20 -0300

    water css moved to Link in HTML

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Router } from './Router'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
