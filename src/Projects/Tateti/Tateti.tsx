93984d22e3cb83808403ac292d775c2c083527e6
LucasScotta <lucasmanuelscotta@gmail.com>
2023-07-20 00:16:41 -0300

    isWinner and isDraw functions added in Tateti Project

--- src/Projects/Tateti/Tateti.tsx
+++ src/Projects/Tateti/Tateti.tsx
@@ -1,8 +1,30 @@
 import { useState } from 'react'
 import { Board } from './Components'
-import './style/main.css'
 import { Player } from './constants'
-
+import './style/main.css'
+type TBoard = (Player | '')[]
+const isWinner = (dif: TBoard, turn: Player) => {
+  for (let i = 0; i < 7; i += 1) {
+    if (i < 3) {
+      const isColumnWin = dif[i] === turn && dif[i + 3] === turn && dif[i + 6] === turn
+      if (isColumnWin) return true
+    }
+    if (i === 0 || i === 3 || i === 6) {
+      const isRowWin = dif[i] === turn && dif[i + 1] === turn && dif[i + 2] === turn
+      if (isRowWin) return true
+    }
+    if (i === 0) {
+      const isDiagonalWin = dif[i] === turn && dif[i + 4] === turn && dif[i + 8] === turn
+      if (isDiagonalWin) return true
+    }
+    if (i === 2) {
+      const isDiagonalWin = dif[i] === turn && dif[i + 2] === turn && dif[i + 4] === turn
+      if (isDiagonalWin) return isDiagonalWin
+    }
+  }
+  return false
+}
+const isDraw = (board: TBoard) => board.every(cell => cell.includes(Player.X) || cell.includes(Player.O))
 const emptyBoard = Array(9).fill('')
 const Tateti = () => {
   const [board, setBoard] = useState<(Player | '')[]>(emptyBoard)
