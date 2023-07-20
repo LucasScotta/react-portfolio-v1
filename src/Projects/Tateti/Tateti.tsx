ea0bb804896fb567eac8ce154173b3b62ca70dc6
LucasScotta <lucasmanuelscotta@gmail.com>
2023-07-19 23:27:58 -0300

    Board in Tateti project styled

--- src/Projects/Tateti/Tateti.tsx
+++ src/Projects/Tateti/Tateti.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import { Board } from './Components'
+import './style/main.css'
 
 const emptyBoard = Array(9).fill('')
 
 const Tateti = () => {
   const [board, setBoard] = useState<string[]>(emptyBoard)
-  return <main>
+  return <main className="Tateti-Project">
+    <h1>Tateti App</h1>
+    <button>Restart</button>
     <Board board={board} />
   </main>
 }
