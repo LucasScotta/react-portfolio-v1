62efbbeb7ca5d9a2d011638a7e1fcb33b26a52b3
LucasScotta <lucasmanuelscotta@gmail.com>
2023-07-20 02:13:11 -0300

    initial state moved to constants in Tateti Project

--- src/Projects/Tateti/constants.ts
+++ src/Projects/Tateti/constants.ts
@@ -1,4 +1,8 @@
+import { Game, TBoard } from './types'
+
 export enum Player {
   X = '❌',
   O = '⚪'
 }
+
+export const INITIAL_STATE: Game = { board: Array(9).fill('') as TBoard, turn: Player.X, scores: { [Player.X]: 0, [Player.O]: 0 }, winner: '', draw: false }
