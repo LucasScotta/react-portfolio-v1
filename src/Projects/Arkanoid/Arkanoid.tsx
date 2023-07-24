0b2a9141a7d8e671247a6a079c0e755befd1b2a9
LucasScotta <lucasmanuelscotta@gmail.com>
2023-07-24 19:38:54 -0300

    start game fn added in Arkanoid Project

--- src/Projects/Arkanoid/Arkanoid.tsx
+++ src/Projects/Arkanoid/Arkanoid.tsx
@@ -1,15 +1,24 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
-import { GameConfig } from './types'
+import { GameConfig, Ball as BallModel } from './types'
 import { INIT_ARKANOID_GAME } from './constants'
 import './styles/main.css'
 import { Button } from '../../Components'
 import { Ball } from './Components'
-
+const createBall = (): BallModel => ({ x: 0, y: 0, angle: 1, height: 10, width: 10, id: 1, speed: 1 })
 /** Arkanoid App Component */
 const Arkanoid = () => {
   const [game, setGame] = useState<GameConfig>({ ...INIT_ARKANOID_GAME })
   const intervalRef = useRef<number | void>()
   const { width: gameWidth, height: gameHeight } = game
+
+  /** Starts the game if it's not started yes */
+  const startGame = () => {
+    const { start } = game
+    if (start) return
+    setGame(prev => {
+      return { ...prev, start: !start, balls: [createBall()] }
+    })
+  }
 
   const update = useCallback(() => {
     () => ''
@@ -31,7 +40,7 @@ const Arkanoid = () => {
       <div className='Arkanoid-handler'>
         {
           !game.start
-            ? <Button onClick={() => setGame({ ...game, start: true })}>START</Button>
+            ? <Button onClick={startGame}>START</Button>
             : <Button onClick={() => setGame({ ...game, pause: !game.pause })}>{game.pause ? 'UNPAUSE' : 'PAUSE'}</Button>
         }
       </div>
@@ -44,7 +53,7 @@ const Arkanoid = () => {
           game.balls.map(ball => <Ball ball={ball} className='Arkanoid-ball' key={ball.id} />)
         }
       </div>
-    </main>
+    </main >
   )
 }
 
