7e7ce7b428fc750094954e3b14c135cab5172576
LucasScotta <lucasmanuelscotta@gmail.com>
2023-07-29 05:08:46 -0300

    styles property added to Button Global Component

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,12 +1,15 @@
-import { FC } from 'react'
+import { CSSProperties, FC } from 'react'
 interface ButtonProps {
   children: string
   onClick?: () => void
   className?: string
+  style?: CSSProperties
 }
-export const Button: FC<ButtonProps> = ({ children, onClick, className }) => {
+export const Button: FC<ButtonProps> = ({ children, onClick, className, style }) => {
   return <button
     onClick={onClick && onClick}
     children={children}
-    className={className && className} />
+    className={className && className}
+    style={style}
+  />
 }
